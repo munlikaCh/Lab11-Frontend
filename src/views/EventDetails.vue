aa485f17fa9064e7a9538f35c1b41278edeee6d5
munlikaCh <munlika_ch@cmu.ac.th>
2021-10-02 16:25:39 +0700

    2.10

--- src/views/EventDetails.vue
+++ src/views/EventDetails.vue
@@ -3,6 +3,7 @@
     <h1>{{ event.title }}</h1>
     <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
     <p>{{ event.description }}</p>
+    <img v-for="url in event.imageUrls" :key="url" :src="url" />
   </div>
 </template>
 
@@ -26,3 +27,16 @@ export default {
   }
 }
 </script>
+<style scoped>
+img {
+  border: 1px solid #ddd; /* Gray border */
+  border-radius: 4px; /* Rounded border */
+  padding: 5px; /* Some padding */
+  margin: 5px; /* Some margin */
+  width: 150px; /* Set a small width */
+}
+/* Add a hover effect (blue shadow) */
+img:hover {
+  box-shadow: 0 0 2px 1px rgba(0, 140, 186, 0.5);
+}
+</style>
